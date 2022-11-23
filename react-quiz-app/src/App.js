659f0b71e6e21384109d49ad417b8ac3bd2e42c8
Joy He <joyhe520@gmail.com>
2022-11-23 11:40:44 -0600

    quiz component hookup

--- react-quiz-app/src/App.js
+++ react-quiz-app/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Settings from './components/Settings';
+import Questions from './components/Questions';
+import FinalScore from './components/FinalScore';
+
 
 const App = () => {
   return (
-    <div>App</div>
+   <Router>
+    <Routes>
+      <Route path="/" element={ <Settings />} />
+      <Route path="/questions" element={ <Questions />} />
+      <Route path="/score" element={ <FinalScore />} />
+    </Routes>
+   </Router>
   )
 }
 
