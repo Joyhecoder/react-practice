7c507413216e268897ceffe655bd5f2d7bc268cc
Joy He <joyhe520@gmail.com>
2022-12-01 13:37:03 -0600

    added redux tool plugs in store.js

--- react-quiz-app/src/pages/Questions.jsx
+++ react-quiz-app/src/pages/Questions.jsx
@@ -8,7 +8,7 @@ const Questions = () => {
   const {
     questions_category,  question_difficulty, question_type, amount_of_question
   } = useSelector(state => state)
-  console.log(amount_of_question)
+  console.log(amount_of_question.amount_of_question, questions_category)
   
   let apiUrl = `/api.php?amount=10`
   const { response, loading } = useAxios({ url: apiUrl })
--- react-quiz-app/src/redux/store.js
+++ react-quiz-app/src/redux/store.js
@@ -1,5 +1,11 @@
-import { createStore} from 'redux';
+import { createStore, applyMiddleware, compose} from 'redux';
 import reducer from './reducer';
 
 
-export default createStore(reducer)
+export default createStore(reducer, 
+    
+      compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+    
+    )
