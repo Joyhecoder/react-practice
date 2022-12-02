b828d19e379e4060d695197103b803102dedd7cc
Joy He <joyhe520@gmail.com>
2022-12-02 12:27:16 -0600

    final screen shows final score and can be navigated back to the main page

--- react-quiz-app/src/pages/FinalScore.jsx
+++ react-quiz-app/src/pages/FinalScore.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Button, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import {useSelector, useDispatch} from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { handleScoreChange, handleAmountChange } from '../redux/actions'
 
 const FinalScore = () => {
+  const score = useSelector(state => state.score)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleBackToSettings = () =>{
+    dispatch(handleScoreChange(0))
+    dispatch(handleAmountChange(50))
+    navigate('/')
+  }
   return (
-    <div>FinalScore</div>
+    <Box mt={30}>
+      <Typography variant="h3" fontWeight="bold" mb={3}>Final Score {score}</Typography>
+      <Button onClick={handleBackToSettings} variant="outlined">Back to settings!</Button>
+    </Box>
   )
 }
 
